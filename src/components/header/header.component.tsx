6bded91b18d37ef193d5ea14b2b93c5f0e1d1040
Pedro Lopes <pedrolopesgomes041@gmail.com>
2023-06-27 16:01:12 -0300

    feat: add Theme Colors file

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,22 +1,23 @@
 import React from "react";
-import "./header.styles.css"
+import "./header.styles.css";
 import { BsCart3 } from "react-icons/bs";
+import { HeaderContainer } from "./header.style";
 
 const Header = () => {
-    return (
-        <div className="header-container">
-                <h2 className="header-title">CLUB CLOTHING</h2>
-                <div className="header-items">
-                    <div className="header-item">Explorar</div>
-                    <div className="header-item">Login</div>
-                    <div className="header-item">Criar Conta</div>
-                    <div className="header-item">
-                        <BsCart3 size={25}/>
-                        <p style={{marginLeft: 5}}>5</p>
-                    </div>
-                </div>
+  return (
+    <HeaderContainer>
+      <h2 className="header-title">CLUB CLOTHING</h2>
+      <div className="header-items">
+        <div className="header-item">Explorar</div>
+        <div className="header-item">Login</div>
+        <div className="header-item">Criar Conta</div>
+        <div className="header-item">
+          <BsCart3 size={25} />
+          <p style={{ marginLeft: 5 }}>5</p>
         </div>
-    )
-}
+      </div>
+    </HeaderContainer>
+  );
+};
 
 export default Header;
