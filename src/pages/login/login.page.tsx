de993dba5b84c6beefabdb26f9ce55e80f7e3ee5
Pedro Lopes <pedrolopesgomes041@gmail.com>
2023-07-07 16:54:26 -0300

    feat: add CustomInput

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,30 +1,50 @@
-import CustomButton from "../../components/custom-button/custom-button";
+import { BsGoogle } from "react-icons/bs";
+import { FiLogIn } from "react-icons/fi";
+
+// Components
 import Header from "../../components/header/header.component";
-import './login.style'
-import { LoginContainer, LoginContent, LoginInputContainer, LoginSubtitle } from "./login.style";
-import {BsGoogle} from 'react-icons/bs'
-import {FiLogIn} from 'react-icons/fi'
+import CustomButton from "../../components/custom-button/custom-button";
+import CustomInput from "../../components/custom-input/custom-input";
 
-const Login = () => {
-    return (
-      <>
-        <Header />
-        <LoginContainer>
-          <LoginContent>
-            <LoginSubtitle>Entre com a sua Conta</LoginSubtitle>
+// Styles
+import {
+  LoginContainer,
+  LoginContent,
+  LoginHeadline,
+  LoginInputContainer,
+  LoginSubtitle,
+} from "./login.style";
 
-            <CustomButton startIcon={<BsGoogle size={18}/>}>Entrar com o Google</CustomButton>
+const LoginPage = () => {
+  return (
+    <>
+      <Header />
 
-            <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
+      <LoginContainer>
+        <LoginContent>
+          <LoginHeadline>Entre com a sua conta</LoginHeadline>
 
-            <LoginInputContainer>{/* email input */}</LoginInputContainer>
-            <LoginInputContainer>{/* password input */}</LoginInputContainer>
+          <CustomButton startIcon={<BsGoogle size={18} />}>
+            Entrar com o Google
+          </CustomButton>
 
-            <CustomButton startIcon={<FiLogIn size={18}/>}>Entrar</CustomButton>
-          </LoginContent>
-        </LoginContainer>
-      </>
-    );
-}
+          <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
 
-export default Login;
+          <LoginInputContainer>
+            <p>E-mail</p>
+            <CustomInput placeholder="Digite seu e-mail" />
+          </LoginInputContainer>
+
+          <LoginInputContainer>
+            <p>Senha</p>
+            <CustomInput placeholder="Digite sua senha" />
+          </LoginInputContainer>
+
+          <CustomButton startIcon={<FiLogIn size={18} />}>Entrar</CustomButton>
+        </LoginContent>
+      </LoginContainer>
+    </>
+  );
+};
+
+export default LoginPage;
