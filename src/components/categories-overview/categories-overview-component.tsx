0acd5584ccbc1310f95de5eb9df4c3adbc010604
Pedro Lopes <pedrolopesgomes041@gmail.com>
2023-07-25 12:48:06 -0300

    fix: update

--- src/components/categories-overview/categories-overview-component.tsx
+++ src/components/categories-overview/categories-overview-component.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import {FunctionComponent, useContext} from 'react'
-import {Container} from "./category.overview";
+import {Container} from "./categories-overview";
 import { CategoryContext } from '../../contexts/category.context'
 
 const CategoriesOverview: FunctionComponent = () => {
