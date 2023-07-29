8bceb59046bdc14808d133db19c829559d23547b
Pedro Lopes <pedrolopesgomes041@gmail.com>
2023-07-29 16:04:37 -0300

    feat: add empty message to cart

--- src/components/cart/cart-component.tsx
+++ src/components/cart/cart-component.tsx
@@ -9,30 +9,39 @@ import {
   CartTotal,
 } from "./cart-styled";
 
-import {FunctionComponent, useContext} from 'react'
-import {BsCartCheck} from 'react-icons/bs'
+import { FunctionComponent, useContext } from "react";
+import { BsCartCheck } from "react-icons/bs";
 
-const Cart:FunctionComponent = () => {
+const Cart: FunctionComponent = () => {
+  const { isVisible, toggleCart, products, productTotalPrice, productsCount } =
+    useContext(CartContext);
 
-    const {isVisible,toggleCart, products,productTotalPrice} = useContext(CartContext)
+  return (
+    <CartContainer isVisible={isVisible}>
+      <CartEscapeArea onClick={toggleCart} />
+      <CartContent>
+        <CartTitle>Seu Carrinho</CartTitle>
 
-    return (
-        <CartContainer isVisible={isVisible}>
-            <CartEscapeArea onClick={toggleCart}/>
-            <CartContent>
-                <CartTitle>Seu Carrinho</CartTitle>
+        {products.map((product) => (
+          <CartItem key={product.id} product={product} />
+        ))}
 
-                {products.map((product) => (
-                    <CartItem key={product.id} product={product}/>
-                ))}
+        {productsCount > 0 && (
+          <>
+            <CartTotal>Total: R${productTotalPrice}</CartTotal>
+            <CustomButton startIcon={<BsCartCheck />}>
+              Ir para o Chekout
+            </CustomButton>
+          </>
+        )}
 
-                <CartTotal>Total: R${productTotalPrice}</CartTotal>
-                <CustomButton startIcon={<BsCartCheck />}>
-                    Ir para o Chekout
-                </CustomButton>
-            </CartContent>
-        </CartContainer>
-    )
-}
+        {productsCount === 0 && (
+            <p>Seu Carrinho está vazio</p>
+        )}
 
-export default Cart
+      </CartContent>
+    </CartContainer>
+  );
+};
+
+export default Cart;
